b6ce7ef2674db224a2c8535d06c00a1f1c70fc66
Peter Qiu <peterqiu1997@gmail.com>
2017-01-12 13:14:07 -0800

    final

--- src/index.js
+++ src/index.js
@@ -23,7 +23,12 @@ class App extends React.Component {
           As Ariel puts it, "WEBMAMA WE LOVE U💖💖💖 u r AMAZING and we appreciate u so much☺️☺️ ty 
           for being awesome💕💕💕🐟🌊."
           <br></br><br></br>
-          John would like to add, "you know what the best kind of ship is...friendship. ily web mom 💖💖💖"
+          John would like to add, "you know what the best kind of ship is...friendship. ily web mom 💖💖💖."
+          <br></br><br></br>
+          Sarah, Catherine, and Ivy send "lotsa love" your way and thank you for being so caring, making time for us,
+          and getting us to bond!
+          <br></br><br></br>
+          Thanks web mama! Enjoy this little slice of the internet. 
         </div>
         <Footer/>
       </div>
